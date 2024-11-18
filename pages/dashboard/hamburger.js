73af0c6a49581d621607a4b3df8969cfe8f45b58
ZeveNor <panthep094@gmail.com>
2024-11-18 07:34:21 +0700

    fix dashboard Layout

--- pages/dashboard/hamburger.js
+++ pages/dashboard/hamburger.js
@@ -4,6 +4,6 @@ hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
 
-$(function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
-});
+// $(function () {
+//   document.querySelector("#sidebar").classList.toggle("expand");
+// });
